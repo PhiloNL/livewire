029dea244fea8299338fec1792d9e14aff889188
Philo Hermans <me@philo.dev>
2023-09-11 19:32:12 +0200

    Add test

--- src/Features/SupportTeleporting/BrowserTest.php
+++ src/Features/SupportTeleporting/BrowserTest.php
@@ -2,7 +2,10 @@
 
 namespace Livewire\Features\SupportTeleporting;
 
+use Illuminate\Support\Facades\Route;
+use Laravel\Dusk\Browser;
 use Livewire\Component;
+use Livewire\Features\SupportPageComponents\ComponentWithTeleport;
 use Livewire\Livewire;
 use Tests\BrowserTestCase;
 
@@ -53,4 +56,50 @@ class BrowserTest extends BrowserTestCase
             ->assertDontSeeIn('@component', 'teleportedbaz')
             ->assertSee('teleportedbaz');
     }
+
+    /** @test */
+    public function can_teleport_full_when_using_full_page_components()
+    {
+        $this->tweakApplication(function() {
+            Livewire::component('full-page-component', FullPageComponent::class);
+            Livewire::component('child-component', ChildComponent::class);
+
+            Route::get('/full-page-component', FullPageComponent::class)->middleware('web');
+        });
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/full-page-component')
+                ->assertSee('I am a form')
+                ->type('@input', 'hello world')
+                ->waitForLivewire()
+                ->assertSee('I am a form');
+        });
+    }
+}
+
+class FullPageComponent extends Component {
+
+    public function render() { return <<<'HTML'
+            <div dusk="component">
+                <button dusk="something" type="button" wire:click="something">
+                    Do Something
+                </button>
+
+                @teleport('body')
+                    <livewire:child-component/>
+                @endteleport
+            </div>
+        HTML; }
+}
+
+class ChildComponent extends Component {
+    public $value;
+
+    public function render() { return <<<'HTML'
+            <form>
+                I am a form
+                <input type="text" dusk="input" wire:model.live="value">
+                <button type="submit">Save</button>
+            </form>
+        HTML; }
 }
